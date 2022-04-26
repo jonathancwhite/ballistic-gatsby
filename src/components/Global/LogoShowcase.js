9b4e73e63176dc4a06008d51b6382da215f14f02
Jonathan White <contact@jonathancwhite.com>
2022-04-26 16:16:06 -0500

    fix for config

--- src/components/Global/LogoShowcase.js
+++ src/components/Global/LogoShowcase.js
@@ -1,20 +1,28 @@
 import React from 'react'
+import { GatsbyImage } from 'gatsby-plugin-image';
 
-const LogoShowcase = ({title,logo1, logo2, logo3, logo4, logo5}) => {
+export default function LogoShowcase(props) {
+  const {
+    title,
+    logo1, 
+    logo2, 
+    logo3, 
+    logo4, 
+    logo5
+  } = props; 
+
   return (
     <div className="logoShowcase">
       <div className="logoShowcase__title">
         <h2>{title}</h2>
       </div>
       <div className="logoShowcase__container">
-          <img src={logo1} height="200" width="100" alt="Brand Logo 1" />
-          <img src={logo2} height="200" width="100" alt="Brand Logo 2" />
-          <img src={logo3} height="200" width="100" alt="Brand Logo 3" />
-          <img src={logo4} height="200" width="100" alt="Brand Logo 4" />
-          <img src={logo5} height="200" width="100" alt="Brand Logo 5" />
+          <GatsbyImage image={logo1} height="200" width="100" alt="Brand Logo 1" />
+          <GatsbyImage image={logo2} height="200" width="100" alt="Brand Logo 2" />
+          <GatsbyImage image={logo3} height="200" width="100" alt="Brand Logo 3" />
+          <GatsbyImage image={logo4} height="200" width="100" alt="Brand Logo 4" />
+          <GatsbyImage image={logo5} height="200" width="100" alt="Brand Logo 5" />
       </div>
     </div>
   )
 }
-
-export default LogoShowcase
