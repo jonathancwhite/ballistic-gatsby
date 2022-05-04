4fa317533bf534149945a84a398f4fde03315e3e
Jonathan White <contact@jonathancwhite.com>
2022-05-04 13:42:16 -0500

    updated index page template

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link, graphql } from "gatsby";
-import { getImage } from "gatsby-plugin-image";
+import { getImage, getSrc } from "gatsby-plugin-image";
 
 import Layout from "../components/Layout";
 import Hero from '../components/Global/Hero';
