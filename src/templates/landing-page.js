bbae4c7b231d20c6448c54d365c54dc54c22f9af
Jonathan White <contact@jonathancwhite.com>
2022-06-20 15:10:37 -0500

    edits to landing page to allow for markdown

--- src/templates/landing-page.js
+++ src/templates/landing-page.js
@@ -1,37 +1,60 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link, graphql } from "gatsby";
-import { getImage } from "gatsby-plugin-image";
-
+import { graphql, Link } from "gatsby";
 import Layout from "../components/Layout";
-import Hero from '../components/Global/Hero';
-import InternalPage from '../components/Global/InternalPage';
+import Content, { HTMLContent } from "../components/Content";
+import Hero from "../components/Global/Hero";
 
 // eslint-disable-next-line
 export const LandingPageTemplate = ({
-
+  hero,
+  content,
+  contentComponent,
+  title,
 }) => {
+  const PostContent = contentComponent || Content;
+  const heroImage = getImage(image) || image;
 
   return (
-    <div>
-      <InternalPage>
-       
-      </InternalPage>
-    </div>
+    <>
+    <Hero 
+      pageTitle={hero.heading}
+      ctaLink={hero.ctaLink}
+      ctaTitle={hero.ctaTitle}
+      img={heroImage}
+      showcaseAlt={hero.imgAlt}
+    />
+    <section className="section">
+      <div className="container content">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              {title}
+            </h1>
+            <PostContent content={content} />
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
   );
 };
 
 LandingPageTemplate.propTypes = {
-
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  title: PropTypes.string,
 };
 
 const LandingPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
+  const { markdownRemark: post } = data;
 
   return (
     <Layout>
       <LandingPageTemplate
-        
+        content={post.html}
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
       />
     </Layout>
   );
@@ -39,17 +62,17 @@ const LandingPage = ({ data }) => {
 
 LandingPage.propTypes = {
   data: PropTypes.shape({
-    markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
+    markdownRemark: PropTypes.object,
   }),
 };
 
 export default LandingPage;
 
 export const pageQuery = graphql`
-  query LandingPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "landing-page" } }) {
+  query LandingPageByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
       frontmatter {
         title
       }
