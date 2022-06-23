52a075eec994e2ca827c24bda5c6eba08ad1fb4a
Jonathan White <contact@jonathancwhite.com>
2022-06-23 15:52:02 -0500

    hubspot form fix

--- src/components/Global/HubspotForm.js
+++ src/components/Global/HubspotForm.js
@@ -4,20 +4,27 @@ import { Helmet } from 'react-helmet';
 
 const HubspotForm = () => {
     useEffect(() => {
-        if (window.hbspt) {
-          window.hbspt.forms.create({
-            portalId: "19492985",
-            formId: "9142183f-8dd5-406c-b144-f08114d57414",
-            target: "#hubspotForm"
-          });
-        }
-      },[window.hbspt])
+        const script = document.createElement('script');
+        script.src='https://js.hsforms.net/forms/v2.js';
+        document.body.appendChild(script);
+
+        script.addEventListener('load', () => {
+            if (window.hbspt) {
+                window.hbspt.forms.create({
+                    portalId: "19492985",
+                    formId: "9142183f-8dd5-406c-b144-f08114d57414",
+                    target: "#hubspotForm"
+                })
+            }
+        });
+    }, []);
+
     return (
     <>
-    <Helmet>
-      <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"> </script>
-    </Helmet>
-    <div id='#hubspotForm'></div>
+        <Helmet>
+        <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"> </script>
+        </Helmet>
+        <div id='#hubspotForm'></div>
     </>
     )
 }
